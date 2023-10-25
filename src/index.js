8fcae2da1f041ddecc0eab49698d1a02440360ff
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-10-25 04:30:19 -0500

    added the data route

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route } from 'react-router-dom';
+import NavBar from './navbar';
+import Home from './home';
+import Credit from './credit';
+import Savings from './savings';
+import Budget from './budget';
+import Register from './register';
+import Login from './login';
+import Data from './data';
+import "./style.css";
+
+const App = () => (
+  <BrowserRouter>
+    <NavBar />
+    <Route exact path='/'>
+      <Home />
+    </Route>
+    <Route exact path='/credit'>
+      <Credit />
+    </Route>
+    <Route exact path='/savings'>
+      <Savings />
+    </Route>
+    <Route exact path='/budget'>
+      <Budget />
+    </Route>
+    <Route exact path='/register'>
+      <Register />
+    </Route>
+    <Route exact path='/logon'>
+      <Login />
+    </Route>
+    <Route exact path='/data'>
+      <Data />
+    </Route>
+  </BrowserRouter>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
