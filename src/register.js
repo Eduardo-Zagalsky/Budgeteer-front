9d8ac2559010ae031a263eac3cc72953b7b1d2af
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-11-15 20:50:29 -0600

    added a redirect function, added a working url to post to the server, reorganized the html to look good with the CSS and added a submit button

--- src/register.js
+++ src/register.js
@@ -1,10 +1,12 @@
 import axios from "axios"
 import { useState } from "react"
+import { useHistory } from "react-router-dom";
 const local = require("localStorage")
-const URL = process.env.FULL_URL;
+const URL = "https://ez-budgeteer.onrender.com";
 const INITIAL_VAL = { name: "", email: "", username: "", password: "", income: "", creditScore: "" }
 
 const Register = () => {
+    const history = useHistory()
     const [formData, setFormData] = useState(INITIAL_VAL);
     const handleChange = (e) => {
         let { name, value } = e.target;
@@ -14,30 +16,41 @@ const Register = () => {
         e.preventDefault();
         const token = await axios.post(`${URL}/signup`, { formData });
         local.setItem("token", token);
+        history.push('/credit-form')
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="name">Full Name: </label>
-                <input type="text" name="name" value={formData.name} onChange={handleChange} />
+                <h2>Sign Up</h2>
+                <div className="boxes">
+                    <input type="text" name="name" value={formData.name} onChange={handleChange} />
+                    <label htmlFor="name">Full Name: </label>
+                </div>
+                <div className="boxes">
+                    <input type="email" name="email" value={formData.email} onChange={handleChange} />
+                    <label htmlFor="email">Email: </label>
+                </div>
+                <div className="boxes">
+                    <input type="text" name="username" value={formData.username} onChange={handleChange} />
+                    <label htmlFor="username">Username: </label>
+                </div>
+                <div className="boxes">
+                    <input type="password" name="password" value={formData.password} onChange={handleChange} />
+                    <label htmlFor="password">Password: </label>
+                </div>
+                <div className="boxes">
+                    <input type="text" name="income" value={formData.income} onChange={handleChange} />
+                    <label htmlFor="income">Income: </label>
+                </div>
+                <div className="boxes">
+                    <input type="text" name="creditScore" value={formData.creditScore} onChange={handleChange} />
+                    <label htmlFor="creditScore">Credit Score: </label>
+                </div>
 
-                <label htmlFor="email">Email: </label>
-                <input type="email" name="email" value={formData.email} onChange={handleChange} />
-
-                <label htmlFor="username">Username: </label>
-                <input type="text" name="username" value={formData.username} onChange={handleChange} />
-
-                <label htmlFor="password">Password: </label>
-                <input type="password" name="password" value={formData.password} onChange={handleChange} />
-
-                <label htmlFor="income">Income: </label>
-                <input type="text" name="income" value={formData.income} onChange={handleChange} />
-
-                <label htmlFor="creditScore">Credit Score: </label>
-                <input type="text" name="creditScore" value={formData.creditScore} onChange={handleChange} />
+                <button type='submit'>Register</button>
             </form>
-        </div>
+        </div >
     )
-}
+};
 export default Register;
