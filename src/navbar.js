852644a9ee8d248cc4d1d3e5b45ed92b8ca81e2e
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2024-02-04 15:01:37 -0600

    created a conditional for the nav bar to show log out or signup and login

--- src/navbar.js
+++ src/navbar.js
@@ -1,13 +1,16 @@
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
+const local = require("localStorage");
 
 const NavBar = () => {
     return (
         <nav>
             <h1 id="title">Budgeteer</h1>
             <NavLink className="nav-link" to="/">Home</NavLink>
-            <NavLink className="nav-link" to="/register">Sign Up</NavLink>
-            <NavLink className="nav-link" to="/login">Log In</NavLink>
+            {local != undefined ? <span><NavLink className="nav-link" to="/">Logout</NavLink></span> : <span><NavLink className="nav-link" to="/register">Sign Up</NavLink>
+                <NavLink className="nav-link" to="/login">Log In</NavLink>
+            </span>}
+
             <NavLink className="nav-link" to="/budget">Budget</NavLink>
             <NavLink className="nav-link" to="/credit">Credit</NavLink>
             <NavLink className="nav-link" to="/savings">Savings</NavLink>
