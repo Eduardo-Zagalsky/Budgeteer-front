928cb25c56e6c4d66be8573614190a66a9630386
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2024-03-06 22:47:17 -0600

    added axios, state, useEffect, localstorage, and a new URL. found User through axios and if present it calls them by name if not it welcomes the nonuser

--- src/home.js
+++ src/home.js
@@ -1,14 +1,32 @@
-const Home = (props) => {
+import axios from "axios";
+import { useState, useEffect } from "react";
+const local = require("localStorage");
+const URL = process.env.REACT_APP_URL;
+const resp = null
+const Home = () => {
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        async function getUser() {
+            const value = local.getItem("token")
+            if (value != null) {
+                resp = await axios.get(`${URL}/`, { headers: { token: value } });
+            }
+            setUser(resp);
+        }
+        getUser();
+    }, []);
     return (
         <div>
-            <h1>Hello {props.user}!</h1>
-            <ul>
-                <h3>Your finances:</h3>
-                <li>Income: {props.income}</li>
-                <li>Expenses: {props.expenses}</li>
-                {props.income - props.expenses >= 0 ? <li>Remaining: {props.remainder}</li> : <li>Short: {props.remainder}</li>}
-                <li>Debts: {props.debts}</li>
-            </ul>
+            {user != null ?
+                <div><h1>Hello {user.full_name}!</h1>
+                    <ul>
+                        <h3>Your finances:</h3>
+                        <li>Income: {user.income}</li>
+                        <li>Expenses: {user.expense}</li>
+                        {/* <li>Debts: {user.credit.balance}</li> */}
+                    </ul>
+                </div>
+                : <h1>Welcome!</h1>}
         </div>
     );
 }
