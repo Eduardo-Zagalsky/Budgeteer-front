700159a706de249361733d3e3805eecb1737e96f
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2024-03-22 20:57:13 -0500

    added useEffect and useState for api handling

--- src/budget.js
+++ src/budget.js
@@ -1,21 +1,27 @@
 import axios from "axios";
+import { useState, useEffect } from "react";
 const local = require("localStorage");
 const URL = process.env.REACT_APP_URL
+let resp;
 
 const Budget = () => {
-    async function getBudget() {
-        const token = local.getItem("token")
-        // const creditInfo = await axios.get(`${URL}/credit`, { headers: token })
-        // return creditInfo;
-    }
-    const credit = getBudget()
+    const [budget, setBudget] = useState(null);
+    useEffect(() => {
+        async function getBudget() {
+            const value = local.getItem("token")
+            if (value != null) {
+                resp = await axios.get(`${URL}/expense`, { headers: { token: value } });
+            }
+            setBudget(resp);
+        }
+        getBudget();
+    }, []);
     return (
         <div>
-            {/* <p>Your income is <b>${tempIncome.toLocaleString()}</b> and your expenses are <b>${tempExpenses.toLocaleString()}</b>.</p>
-            <h4>Currently your DTI is {(tempExpenses / tempIncome * 100)}%</h4> */}
+            <p>Your income is <b>${budget.name}</b> and your expenses are <b>${budget.amount}</b>.</p>
+            <h4>Currently your DTI is {(budget.amount * 100)}%</h4>
             <p>Ideally you would want your expenses to be less than 45% of your income, so let's try to come up with some ways to make that true!
                 Let's see your expenses and decide which of these you could cut back on to try to get on the best path to financial freedom.</p>
-            {/* {for(let x in expenses)} */}
         </div>
     )
 }
