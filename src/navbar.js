73cf230586a9871cae9ef7e27db5a4887bc1523b
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2024-04-25 21:15:17 -0500

    removed useHistory and set navlink to login page,deleted the setitem line

--- src/navbar.js
+++ src/navbar.js
@@ -1,21 +1,18 @@
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
-import { useHistory } from "react-router-dom";
 const local = require("localStorage");
 
 const NavBar = () => {
-    const history = useHistory();
     const logout = () => {
         local.removeItem('token');
-        local.setItem("token", null);
-        history.push("/")
+        console.log(local.getItem('token'));
     }
 
     return (
         <nav>
             <h1 id="title">Budgeteer</h1>
             <NavLink className="nav-link" to="/">Home</NavLink>
-            {local.getItem("token") != null ? <span><NavLink className="nav-link" to={logout}>Logout</NavLink></span> : <span><NavLink className="nav-link" to="/register">Sign Up</NavLink>
+            {local.getItem("token") !== null ? <span><NavLink className="nav-link" to="/login" onClick={logout}>Logout</NavLink></span> : <span><NavLink className="nav-link" to="/register">Sign Up</NavLink>
                 <NavLink className="nav-link" to="/login">Log In</NavLink>
             </span>}
 
