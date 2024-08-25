1b3a20ad0cfdd2af70d005b75c3c296ab225116a
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2024-08-25 12:25:33 -0500

    imported userContext, set user context to null to for the purpose of logging out, set nav links conditionally on whether youre logged in or not

--- src/navbar.js
+++ src/navbar.js
@@ -1,24 +1,27 @@
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
+import { useContext } from "react";
+import UserContext from "./context";
 const local = require("localStorage");
 
 const NavBar = () => {
+    let user = useContext(UserContext)
     const logout = () => {
         local.removeItem('token');
         console.log(local.getItem('token'));
+        user.setUser(null)
     }
-
     return (
         <nav>
             <h1 id="title">Budgeteer</h1>
             <NavLink className="nav-link" to="/">Home</NavLink>
-            {local.getItem("token") !== null ? <span><NavLink className="nav-link" to="/login" onClick={logout}>Logout</NavLink></span> : <span><NavLink className="nav-link" to="/register">Sign Up</NavLink>
+            {user['user'] !== null ? <span><NavLink className="nav-link" to="/login" onClick={logout}>Logout</NavLink>
+                <NavLink className="nav-link" to="/budget">Budget</NavLink>
+                <NavLink className="nav-link" to="/credit">Credit</NavLink>
+                <NavLink className="nav-link" to="/savings">Savings</NavLink>
+            </span> : <span><NavLink className="nav-link" to="/register">Sign Up</NavLink>
                 <NavLink className="nav-link" to="/login">Log In</NavLink>
             </span>}
-
-            <NavLink className="nav-link" to="/budget">Budget</NavLink>
-            <NavLink className="nav-link" to="/credit">Credit</NavLink>
-            <NavLink className="nav-link" to="/savings">Savings</NavLink>
         </nav>
     );
 }
