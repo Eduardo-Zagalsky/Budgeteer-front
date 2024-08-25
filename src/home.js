a7edb578a14f0d038da7846123bfa9b8bc5fafef
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2024-08-25 11:30:14 -0500

    added useContext,left notes for myself, renamed thevariable to data and the function, added conditionals for displaying welcome message

--- src/home.js
+++ src/home.js
@@ -1,29 +1,36 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import UserContext from "./context";
 const local = require("localStorage");
 const URL = process.env.REACT_APP_URL;
 let resp = null
+
+// user=data[0];
+// expenses=data[1][0];
+// credit=data[2][0];
+// account=data[3][0];
+
 const Home = () => {
-    const [user, setUser] = useState(null);
+    const [data, setData] = useState(null);
     useEffect(() => {
-        async function getUser() {
+        async function getData() {
             const value = local.getItem("token")
             if (value != null) {
-                resp = await axios.get(`${URL}/`, { headers: { token: value } });
-                
-                setUser(resp.data);
+                resp = await axios.get(`${URL}/home`, { headers: { token: value } });
+                setData(resp.data);
             }
         }
-        getUser();
+        getData();
     }, []);
+    let user = useContext(UserContext)
     return (
         <div>
-            {user !== null ?
-                <div><h1>Hello {user.full_name}!</h1>
+            {user['user'] !== null && data !== null ?
+                <div><h1>Hello {user["user"].full_name}!</h1>
                     <ul>
                         <h3>Your finances:</h3>
-                        <li>Income: {user.income}</li>
-                        <li>Expenses: {user.expense}</li>
+                        <li>Income: {data[3][0].balance}</li>
+                        <li>Expenses: {data[1][0].amount}</li>
                     </ul>
                 </div>
                 : <h1>Welcome!</h1>}
