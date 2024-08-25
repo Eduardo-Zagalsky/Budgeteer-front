914ed0077b3dffa5e24d13c79cd07a676013bf74
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2024-08-25 11:41:33 -0500

    added userContext and provider. imported axios, useState and useEffect. got the local storage and URL. used UserData to give value to the user for userContext

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import UserContext from './context';
+import axios from 'axios';
 import NavBar from './navbar';
 import Home from './home';
 import Credit from './credit';
@@ -12,39 +15,62 @@ import CreditForm from './credit-form';
 import AccountForm from './account-form';
 import ExpenseForm from './expense-form';
 import "./style.css";
+const local = require("localStorage");
+const URL = process.env.REACT_APP_URL;
+let resp;
 
-const App = () => (
-  <BrowserRouter>
-    <NavBar />
-    <Route exact path='/'>
-      <Home />
-    </Route>
-    <Route exact path='/credit'>
-      <Credit />
-    </Route>
-    <Route exact path='/savings'>
-      <Savings />
-    </Route>
-    <Route exact path='/budget'>
-      <Budget />
-    </Route>
-    <Route exact path='/register'>
-      <Register />
-    </Route>
-    <Route exact path='/login'>
-      <Login />
-    </Route>
-    <Route exact path='/credit-form'>
-      <CreditForm />
-    </Route>
-    <Route exact path='/account-form'>
-      <AccountForm />
-    </Route>
-    <Route exact path='/expense-form'>
-      <ExpenseForm />
-    </Route>
-  </BrowserRouter>
-)
+
+
+const App = () => {
+  const [user, setUser] = useState(null);
+  const UserData = () => {
+    useEffect(() => {
+      async function getUser() {
+        const value = local.getItem("token")
+        if (value != null) {
+          resp = await axios.get(`${URL}/`, { headers: { token: value } });
+          setUser(resp.data);
+        }
+      }
+      getUser();
+    }, []);
+    return user;
+  }
+  UserData()
+  return(
+  <UserContext.Provider value={{user,setUser}}>
+    <BrowserRouter>
+      <NavBar />
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route exact path='/credit'>
+        <Credit />
+      </Route>
+      <Route exact path='/savings'>
+        <Savings />
+      </Route>
+      <Route exact path='/budget'>
+        <Budget />
+      </Route>
+      <Route exact path='/register'>
+        <Register />
+      </Route>
+      <Route exact path='/login'>
+        <Login />
+      </Route>
+      <Route exact path='/credit-form'>
+        <CreditForm />
+      </Route>
+      <Route exact path='/account-form'>
+        <AccountForm />
+      </Route>
+      <Route exact path='/expense-form'>
+        <ExpenseForm />
+      </Route>
+    </BrowserRouter>
+  </UserContext.Provider >
+)}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
